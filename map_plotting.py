3202413f4b4a562199ed43bfe8ed5832d699d36c
tplay <t.playsted@gmail.com>
2023-04-24 17:53:06 +1000

    Week 8 tasks

--- map_plotting.py
+++ map_plotting.py
@@ -9,6 +9,7 @@ from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 from itinerary import Itinerary
 from city import City
+import numpy as np
 
 
 def plot_itinerary(itinerary: Itinerary, projection='robin', line_width=2, colour='b') -> None:
@@ -24,6 +25,33 @@ def plot_itinerary(itinerary: Itinerary, projection='robin', line_width=2, colou
     :param colour: The colour of the line to draw.
     """
     # TODO
+
+    # create bounding box
+    max_lat = max([city.coordinates[0] for city in itinerary.cities])
+    max_lon = max([city.coordinates[1] for city in itinerary.cities])
+
+    min_lat = min([city.coordinates[0] for city in itinerary.cities])
+    min_lon = min([city.coordinates[1] for city in itinerary.cities])
+
+    # lon_0 is central longitude of projection.
+    # resolution = 'c' means use crude resolution coastlines.
+    m = Basemap(projection=projection, resolution='c', lon_0=0, lat_0=0)
+    m.drawcoastlines()
+    m.fillcontinents(color='coral', lake_color='aqua')
+    # draw parallels and meridians.
+    m.drawparallels(np.arange(-90., 120., 30.))
+    m.drawmeridians(np.arange(0., 360., 60.))
+    m.drawmapboundary(fill_color='aqua')
+
+    for i in range(len(itinerary.cities) - 1):
+        lon1 = itinerary.cities[i].coordinates[1]
+        lat1 = itinerary.cities[i].coordinates[0]
+        lon2 = itinerary.cities[i+1].coordinates[1]
+        lat2 = itinerary.cities[i+1].coordinates[0]
+        m.drawgreatcircle(lon1, lat1, lon2, lat2, linewidth=2, color=colour)
+
+    plt.title("Itinerary")
+    plt.show()
 
 
 if __name__ == "__main__":
